6d23af72ffc806e99a57aa49e28b1265e82fa640
lscon <lscon@users.noreply.github.com>
2018-03-15 11:51:31 -0300

    Criando o activity proprietário

---
+++ app/src/main/java/br/edu/iff/pooa20172/trabalhooficina/activity/ProprietarioActivity.java
@@ -0,0 +1,78 @@
+package br.edu.iff.pooa20172.trabalhooficina.activity;
+
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.List;
+
+import br.edu.iff.pooa20172.trabalhooficina.R;
+import br.edu.iff.pooa20172.trabalhooficina.adapter.ClickRecyclerViewListener;
+import br.edu.iff.pooa20172.trabalhooficina.adapter.ProprietarioAdapter;
+import br.edu.iff.pooa20172.trabalhooficina.model.Proprietario;
+import io.realm.Realm;
+
+public class ProprietarioActivity extends AppCompatActivity implements ClickRecyclerViewListener {
+
+    private Realm realm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_proprietario);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        realm = Realm.getDefaultInstance();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProprietarioActivity.this,ProprietarioDetalheActivity.class);
+                intent.putExtra("id",0);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private List<Proprietario> getProprietarios(){
+
+        return (List)realm.where(Proprietario.class).findAll();
+
+    }
+
+    @Override
+    public void onClick(Object object) {
+        Proprietario proprietario = (Proprietario) object;
+        Intent intent = new Intent(ProprietarioActivity.this,ProprietarioDetalheActivity.class);
+        intent.putExtra("id",proprietario.getId());
+        startActivity(intent);
+
+    }
+
+    protected void onResume() {
+        super.onResume();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.rv_Proprietario);
+
+        recyclerView.setAdapter(new ProprietarioAdapter(getProprietarios(),this,this));
+        RecyclerView.LayoutManager layout = new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false);
+
+        recyclerView.setLayoutManager(layout);
+
+
+    }
+
+    @Override
+    public void finish(){
+        realm.close();
+    }
+
+}
